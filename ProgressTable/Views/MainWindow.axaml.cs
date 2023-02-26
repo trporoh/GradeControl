31e575d0ad57b1ec269f5f3f88cc7eea8d5e7869
trporoh <99017248+trporoh@users.noreply.github.com>
2023-02-26 23:50:06 +0700

    add Controller himself

---
+++ ProgressTable/Views/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using ProgressTable.Models;
+
+namespace ProgressTable.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
